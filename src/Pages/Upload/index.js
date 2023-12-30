fd5bed30e21c03429ed0c7da3c6c59bb7a09ea03
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2023-12-30 22:19:18 +0700

    commit

---
+++ src/Pages/Upload/index.js
@@ -0,0 +1,7 @@
+import Header from '~/components/Layout/components/Header';
+
+function Upload() {
+    return <h2>Upload page</h2>;
+}
+
+export default Upload;
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/Pages/Home';
+import Following from '~/Pages/Following';
+import Profile from '~/Pages/Profile';
+import Upload from '~/Pages/Upload';
+import Search from '~/Pages/Search';
+
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+export const privateRoutes = [];
